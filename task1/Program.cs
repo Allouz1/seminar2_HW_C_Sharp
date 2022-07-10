876592fb3eb37ad5751a3bffbd9c7c15722024d3
Max <makism.mush21@gmail.com>
2022-07-10 13:31:29 +0300

    complit task1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ведите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string str = number.ToString();
+int result = Convert.ToInt32(str[1].ToString());
+for (int i = 1; i < str.Length; i++ )
+{
+    if (result == Convert.ToInt32(str[i].ToString()))
+    {
+    result = Convert.ToInt32(str[i].ToString());
+    }
+}
+Console.WriteLine("Result: " + result);
