3a11b2c081761719d33fcbb179eb3b3af4c12f04
Max <makism.mush21@gmail.com>
2022-07-12 15:45:32 +0300

    done task3

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите числе: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int weekday = 5;
+if (number > weekday)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
