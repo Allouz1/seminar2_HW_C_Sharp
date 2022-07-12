4ebf4c415d258ba877e44cb08551d120f10339f6
Max <makism.mush21@gmail.com>
2022-07-12 15:45:08 +0300

    done task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,17 +2,22 @@
 int number = Convert.ToInt32(Console.ReadLine());
 string str = number.ToString();
 int i = 0;
-int result = Convert.ToInt32(str[2].ToString());//Не понял как работать с этой функцией
-while (i < str.Length)
+// int result = Convert.ToInt32(str[2].ToString());
+if (number > 99)
 {
+    int result = Convert.ToInt32(str[2].ToString());
     
     if (result == Convert.ToInt32(str[i].ToString()))
-    {
-    result = Convert.ToInt32(str[i].ToString());
-    }
-    i++;
+        {
+            result = Convert.ToInt32(str[i].ToString());
+            
+        }
+    Console.WriteLine("Result: " + result);
 }
- Console.WriteLine("Result: " + result);
+else
+{
+    Console.WriteLine("Нет третьей цифры");
+}
  
- // Я понял что здача по стректуре как и первая, не не понимаю как работать с функцией,
- // и если я ставлю число меньше то он выдаёт ошибку.
+ // Я понял, что задача по структуре как и первая, но не понимаю как работать с функцией,
+ // и если я ставлю число меньше, то он выдаёт ошибку.
